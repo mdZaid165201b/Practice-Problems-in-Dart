9124f1b28a9f284bfac74df2721952fccbab0b17
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-08-30 20:45:40 +0500

    Update checkAngram.dart

--- checkAngram.dart
+++ checkAngram.dart
@@ -1,12 +1,12 @@
-
 bool? checkAngram(String s1,String s2){
   if(s1.length != s2.length) return false;
+  
   List<String> s1Array = s1.split('');
   List<String> s2Array = s2.split('');
+  
   bool isTrue = false;
   s2Array.forEach((elem) {
     isTrue = s1Array.contains(elem);
-
   });
   return isTrue;  
 }
@@ -15,5 +15,4 @@ void main() {
   print(checkAngram("test","ttew"));
   if(checkAngram("Listen","Silent")!) print("Angram");
   else{print("Not Angram");}
-  
 }
