e0f35260922e2c333b1f7a6044de8aa645e95221
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-08-31 21:47:09 +0500

    FInd Missing Number in array
    Question Taken From GeeksForGreeks Practice Problem of Array

---
+++ findMissingNumber.dart
@@ -0,0 +1,15 @@
+int? findMissingNumber(List<int>? list){
+  list!.sort();
+  int next = list[1];
+  for(int i  = 0;i < list.length-1; i++){
+    if(list[i+1] != next){return next;}
+    else{
+      next++;
+    }
+  }
+}
+
+void main(){
+  List<int>? list = [6,1,2,8,3,4,7,10,5];
+  print(findMissingNumber(list));
+}
